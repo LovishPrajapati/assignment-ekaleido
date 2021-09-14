d1dbadf05c5c1e9eab65ff349339aca3872f30ab
LovishPrajapati <prajapatilovish@gmail.com>
2021-09-14 15:36:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import data from "./data.json";
 
 function App() {
+  const [currentQue, setCurrentQue] = useState(0);
+
+  const submitHandler = (e, que) => {
+    e.preventDefault();
+    if (e.target.id === que.answerId) {
+      e.target.style.backgroundColor = "green";
+      console.log(que);
+      setCurrentQue((pre) => pre + 1);
+    } else {
+      e.target.style.backgroundColor = "red";
+      document.getElementById(que.id + que.answerId).style.backgroundColor =
+        "green";
+      setCurrentQue((pre) => pre + 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Carousel
+        className="slider"
+        useKeyboardArrows={false}
+        swipeable={false}
+        showThumbs={false}
+        showIndicators={false}
+        showArrows={false}
+        selectedItem={currentQue}
+      >
+        {data.map((que) => (
+          <div className="container">
+            <div key={que.id} className="question">
+              <p>{que.question}</p>
+            </div>
+            <div className="options">
+              {que.options.map((opt) => (
+                <button
+                  key={opt.id}
+                  id={que.id + opt.id}
+                  onClick={(e) => submitHandler(e, que)}
+                >
+                  {opt.option}
+                </button>
+              ))}
+            </div>
+          </div>
+        ))}
+      </Carousel>
+      <button id="reset" onClick={() => window.location.reload()}>
+        Reset
+      </button>
+    </>
   );
 }
 
